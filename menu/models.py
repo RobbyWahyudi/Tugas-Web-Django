8079e88cb4411dd4edbc9bd65cc639814e85f103
RobbyWahyudi <127503546+RobbyWahyudi@users.noreply.github.com>
2023-10-23 05:22:07 +0700

    upload web django project

---
+++ menu/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Produk(models.Model):
+    nama = models.CharField(max_length=255)
+    harga = models.IntegerField()
---
+++ menu/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+
+def home(request):
+    template = loader.get_template("home.html")
+    return HttpResponse(template.render())
+
+
+def products(request):
+    template = loader.get_template("products.html")
+    return HttpResponse(template.render())
+
+
+def contact(request):
+    template = loader.get_template("contact.html")
+    return HttpResponse(template.render())
+
+
+def about(request):
+    template = loader.get_template("about.html")
+    return HttpResponse(template.render())
